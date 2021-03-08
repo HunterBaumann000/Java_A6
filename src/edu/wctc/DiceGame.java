c6cc45ad746b1833dc080121e86989d3dda5aa9a
HunterBaumann000 <71730976+HunterBaumann000@users.noreply.github.com>
2021-03-07 23:28:04 -0600

    Initial commit

---
+++ src/edu/wctc/DiceGame.java
@@ -0,0 +1,168 @@
+package edu.wctc;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class DiceGame {
+
+    private final List<Player> players = new ArrayList<>();
+    private final List<Die> dice = new ArrayList<>();
+    private final int maxRolls;
+    private Player currentPlayers;
+
+    public DiceGame(int countPlayers, int countDice, int maxRolls) {
+        List<Player> players = this.players;
+        List<Die> dice = this.dice;
+        this.maxRolls = maxRolls;
+
+        for(int i = 0; i < countPlayers) {
+            countDice++;
+            maxRolls++;
+            Player player = new Player();
+            Die die = new Die(6);
+        }
+
+        try {
+            if(countPlayers < 2) {
+                throw new IllegalArgumentException();
+            }
+
+        } catch(IllegalArgumentException e){
+            //System.out.println("There needs at least two players");
+        }
+    }
+
+    private boolean allDiceHeld() {
+        boolean isHeld = dice.stream()
+                .allMatch(Die::isBeingHeld);
+
+        if (isHeld) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean autoHold(int faceValue) {
+        boolean autoHold = dice.stream()
+                .filter(Die::isBeingHeld)
+                .findFirst(faceValue)
+                .isPresent();
+
+        if (autoHold) {
+            if(!allDiceHeld()){
+                return true;
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean currentPlayerCanRoll() {
+        if (maxRolls > 0 && !(allDiceHeld())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int getCurrentPlayerNumber() {
+        return players.size();
+    }
+
+    public int getCurrentPlayerScore() {
+
+        return players.get(1).getScore();
+    }
+
+    public String getDiceResults() {
+        String diceResults = dice.stream()
+                .map(Die::toString)
+                .collect(Collectors.joining());
+
+        return diceResults;
+    }
+
+    public String getFinalWinner(){
+        int finalWinner =
+                (int) Collections.max(getCurrentPlayerScore(), Comparator.comparingInt(Player::getScore));
+
+        return "The winner is player " + finalWinner;
+
+    }
+
+    public String getGameResults() {
+        var gameResults = players.stream()
+                .max(Comparator.reverseOrder())
+                .forEach(Player::getPlayerNumber)
+                .map(String::valueOf)
+                .collect(Collectors.joining(" | "));
+
+        return "Game Results: " + gameResults;
+    }
+
+    private boolean isHoldingDie(int faceValue) {
+        var holdingDie = dice.stream()
+                .filter(Die::isBeingHeld)
+                .findFirst(dice.equals(faceValue))
+                .isPresent();
+
+        if(holdingDie) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean nextPlayer(){
+        if(players.size() > players.size()){
+            this.currentPlayer = currentPlayer;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void playerHold(char dieNum){
+        dieNum = dice.stream()
+                .filter(isHoldingDie())
+                .findFirst(die -> dieNum)
+                .isPresent();
+    }
+
+    public void resetDice(){
+        diceReset = dice.stream()
+                .forEach(Die::resetDie);
+
+        dice.clear();
+    }
+
+    public void resetPlayers(){
+        playerReset = players.stream()
+                .forEach(Player::resetPlayer);
+
+        players.clear();
+    }
+
+    public void rollDice(){
+        roll = dice.stream()
+                .forEach(Die::rollDie);
+    }
+
+    public void scoreCurrentPlayer() {
+
+        if(currentPlayers.getRollsUsed() == 6 ||
+           currentPlayers.getRollsUsed() == 5 ||
+           currentPlayers.getRollsUsed() == 4 )
+        {
+          currentPlayers.setScore(getCurrentPlayerScore() + currentPlayers.getRollsUsed());
+        }
+    }
+
+    public void startNewGame() {
+        players.sort(Comparator.comparing(Player::getScore));
+        players.set(0,players.get(0));
+        resetPlayers();
+    }
+}
